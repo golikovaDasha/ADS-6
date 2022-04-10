f57b8ad97da35ba6abb8800c127db1229e7d9f82
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-04-10 15:35:56 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
@@ -14,7 +15,7 @@ if ((first - last) >= 0) {
   throw std::string ("Full");
 } else {
   int i = last;
-  while (i <= last && arr[i % size].prior > arr[temp].prior) {
+  while (i <= last && arr[i % size].prior > temp.prior) {
     arr[(i + 1) % size] = arr[i % size];
     i -= 1;
   }
@@ -22,7 +23,7 @@ arr[(i + 1) % size] = temp;
 last += 1;
 }	
 }
-int pop() {
+T pop() {
   return arr[(first++) % size];
 }
 };

343a4df0d8921c5b7741c736d65c2bf1a7af95a0
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-04-10 15:19:45 +0300

    Голикова 21-ИСТ-3

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,25 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
-};
+private: T arr[size];
+  int first, last;
 
+public: 
+  void push(T x) {
+  i = last;
+  while (i < last && arr[i % size].prior > arr[x].prior) {
+    arr[(i + 1) % size] = arr[i % size];
+    i -= 1;
+}
+arr[(i + 1) % size] = x;
+last += 1;	
+}
+int pop() {
+  return arr[(first++) % size];
+}
+};
 struct SYM {
   char ch;
   int prior;
 };
-
 #endif  // INCLUDE_TPQUEUE_H_
